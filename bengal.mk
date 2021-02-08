94ef6c88a7db17bb5b56daf06beb4c4d0fad0a46
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-08 09:24:01 +0100

    sm6115-common: Don't build full super image
    Change-Id: I40ebb9c756c3546ef6f3de922a24c0660382932a

--- bengal.mk
+++ bengal.mk
@@ -3,5 +3,9 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sm6115-common/sm6115-common-vendor.mk)
