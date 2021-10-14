842e0208e91bf66041ae3afa603bb2ea4c219ac4
Uptownboys32 <anggaalfian133@gmail.com>
2021-10-14 20:59:29 +0700

    sm6115-common: Import overlay

--- bengal.mk
+++ bengal.mk
@@ -7,6 +7,12 @@
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
