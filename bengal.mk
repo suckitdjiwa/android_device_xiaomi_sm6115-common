4ee9b03b7aaf140ccdde3a0db1dd0f74b32c6361
SebaUbuntu <barezzisebastiano@gmail.com>
2021-10-14 11:07:31 +0700

    sm6115-common: Initial skeleton
    Signed-off-by: Uptownboys32 <anggaalfian133@gmail.com>

---
+++ bengal.mk
@@ -0,0 +1,7 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Inherit the proprietary files
+$(call inherit-product, vendor/xiaomi/sm6115-common/sm6115-common-vendor.mk)
