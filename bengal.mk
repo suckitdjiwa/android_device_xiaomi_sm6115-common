91be8614471dd65fccb9f9a3cff4664fbcf4ea5c
Uptownboys32 <anggaalfian133@gmail.com>
2021-10-15 08:27:08 +0700

    sm6115-common: Import keylayout configs

--- bengal.mk
+++ bengal.mk
@@ -220,6 +220,12 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
+
 # Lights
 PRODUCT_PACKAGES += \
     lights.bengal
